97f6da04fde9aec2a9aad03a302aee41e69c8cd3
Shaun Coulter <shaundcoulter@gmail.com>
2021-07-14 20:18:04 -0400

    begin working on SQL setup for database, need to pull all tables

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+-- Creating main table.
+CREATE TABLE main_ds (
+	country VARCHAR NOT NULL,
+	year INT NOT NULL,
+	life_ladder FLOAT,
+	temperature FLOAT,
+	clean_water FLOAT,
+	greenhouse FLOAT,
+	population FLOAT,
+	pm25 FLOAT
+);
