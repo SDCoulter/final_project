3ff8c09b47a2ee97359bff00b015379d93027b2e
Shaun Coulter <shaundcoulter@gmail.com>
2021-07-22 18:17:10 -0400

    added files for machine learning and sql database

---
+++ sql_database/queries.sql
@@ -0,0 +1,70 @@
+-- Country Year List
+SELECT row_number() OVER () AS id,
+		ds.country,
+		ds.year
+INTO country_year
+FROM main_ds AS ds
+-- Add primary key.
+ALTER TABLE country_year ADD PRIMARY KEY (id);
+
+
+-- Country/Year Populations and Clean Water Access --
+SELECT cyp.country, cyp.year, cyp.population, cycw.clean_water
+INTO cy_pop_cw
+FROM cy_population AS cyp
+LEFT JOIN cy_clean_water AS cycw
+ON cyp.country = cycw.country AND cyp.year = cycw.year;
+
+SELECT * FROM cy_pop_cw;
+
+
+-- G7 Countries.
+SELECT *
+INTO g7_table
+FROM cy_greenhouse
+WHERE country = 'United States' OR
+		country = 'United Kingdom' OR
+		country = 'Canada' OR
+		country = 'France' OR
+		country = 'Germany' OR
+		country = 'Italy' OR
+		country = 'Japan';
+-- Remove the greenhouse column so only the countries and years remain.
+ALTER TABLE g7_table
+DROP COLUMN greenhouse;
+
+
+-- Create the main table through multiple joins.
+-- Include unique ID.
+SELECT row_number() OVER () AS id,
+		cw.country,
+		cw.year,
+		cw.clean_water,
+		co2.co2_emissions,
+		gg.greenhouse,
+		ll.life_ladder,
+		pm.pm25,
+		pop.population,
+		temps.temperature
+INTO main_joined_table
+FROM cy_clean_water AS cw
+LEFT JOIN cy_co2_emissions AS co2
+	ON cw.country = co2.country AND cw.year = co2.year
+LEFT JOIN cy_greenhouse AS gg
+	ON cw.country = gg.country AND cw.year = gg.year
+LEFT JOIN cy_life_ladder AS ll
+	ON cw.country = ll.country AND cw.year = ll.year
+LEFT JOIN cy_pm25 AS pm
+	ON cw.country = pm.country AND cw.year = pm.year
+LEFT JOIN cy_population AS pop
+	ON cw.country = pop.country AND cw.year = pop.year
+LEFT JOIN cy_temperature AS temps
+	ON cw.country = temps.country AND cw.year = temps.year;
+
+
+-- Join data to G7 table query air quality for 2012.
+SELECT g7.country, ds.pm25, ds.co2_emissions, ds.greenhouse
+FROM g7_table AS g7
+LEFT JOIN main_joined_table AS ds
+	ON g7.country = ds.country AND g7.year = ds.year
+WHERE g7.year = 2012;
---
+++ sql_database/schema.sql
@@ -0,0 +1,190 @@
+-- MAIN DATASET --
+CREATE TABLE main_ds (
+	country VARCHAR NOT NULL,
+	year INT NOT NULL,
+	life_ladder FLOAT,
+	temperature FLOAT,
+	clean_water FLOAT,
+	greenhouse FLOAT,
+	population FLOAT,
+	pm25 FLOAT,
+	co2_emissions FLOAT
+);
+
+-- MAIN DATASET WITH PRIMARY KEY --
+CREATE TABLE main_ds_pk (
+	id INT NOT NULL,
+	country VARCHAR NOT NULL,
+	year INT NOT NULL,
+	life_ladder FLOAT,
+	temperature FLOAT,
+	clean_water FLOAT,
+	greenhouse FLOAT,
+	population FLOAT,
+	pm25 FLOAT,
+	co2_emissions FLOAT,
+	PRIMARY KEY (id)
+);
+
+-- MAIN DATASET FOR PER CAPITA COLUMNS --
+CREATE TABLE main_pcapita (
+	"ID" INT NOT NULL,
+	"Country" VARCHAR,
+	"Year" INT,
+	"Life Ladder" FLOAT,
+	"Temperature" FLOAT,
+	"Clean Water" FLOAT,
+	"PM2.5" FLOAT,
+	"pc Greenhouse Gas Emissions" FLOAT,
+	"pc CO2 Emissions" FLOAT,
+	PRIMARY KEY ("ID")
+);
+
+-- TABLES FOR COUNTRY-YEAR INDEXES --
+CREATE TABLE cy_clean_water (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  clean_water FLOAT
+);
+
+CREATE TABLE cy_co2_emissions (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  co2_emissions FLOAT
+);
+
+CREATE TABLE cy_greenhouse (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  greenhouse FLOAT
+);
+
+CREATE TABLE cy_life_ladder (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  life_ladder FLOAT
+);
+
+CREATE TABLE cy_pm25 (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  pm25 FLOAT
+);
+
+CREATE TABLE cy_population (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  population FLOAT
+);
+
+CREATE TABLE cy_temperature (
+  country VARCHAR NOT NULL,
+  year int NOT NULL,
+  temperature FLOAT
+);
+
+-- TABLES FOR COUNTRY ONLY INDEXES --
+CREATE TABLE country_clean_water (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  "2019" FLOAT,
+  PRIMARY KEY (country)
+);
+
+CREATE TABLE country_co2_emissions (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  "2019" FLOAT,
+  PRIMARY KEY (country)
+);
+
+CREATE TABLE country_greenhouse (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  PRIMARY KEY (country)
+);
+
+CREATE TABLE country_life_ladder (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  "2019" FLOAT,
+  "2021" FLOAT,
+  PRIMARY KEY (country)
+);
+
+CREATE TABLE country_pm25 (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  "2019" FLOAT,
+  PRIMARY KEY (country)
+);
+
+CREATE TABLE country_population (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  "2019" FLOAT,
+  PRIMARY KEY (country)
+);
+
+CREATE TABLE country_temperature (
+  country VARCHAR NOT NULL,
+  "2010" FLOAT,
+  "2011" FLOAT,
+  "2012" FLOAT,
+  "2013" FLOAT,
+  "2014" FLOAT,
+  "2015" FLOAT,
+  "2016" FLOAT,
+  "2017" FLOAT,
+  "2018" FLOAT,
+  "2019" FLOAT,
+  PRIMARY KEY (country)
+);
